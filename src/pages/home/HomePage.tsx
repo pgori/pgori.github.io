f97f43c25196696f5429fe15d7f96e282f017ce4
Pietro <pietro_gori@hotmail.com>
2025-08-06 17:58:35 -0300

    added motion (animation library)

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,25 @@
+import { motion } from "motion/react"
+
 function HomePage() {
-    return <h1>Hello there, you are at the home page.</h1>
+    // return <h1 className="text-3xl font-bold text-blue-800">Hello there, you are at the home page.</h1>
+    return Rotate()
+}
+
+function Rotate() {
+    return (
+        <motion.div
+            style={box}
+            animate={{ rotate: 360 }}
+            transition={{ duration: 1 }}
+        />
+    )
+}
+
+const box = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#ff0088",
+    borderRadius: 5,
 }
 
 export default HomePage;
