77ec67a05d3329c2d56621cad4ab49a801f55b42
Pietro <pietro_gori@hotmail.com>
2025-08-13 17:30:24 -0300

    removed tailwind utilities import, and base; keeping only the tailwindcss import; css adjustments; added personal description;

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,17 +1,22 @@
 import myPicture from '@/assets/images/pietro-arnold-sports.jpg';
+import VoronoiBackground from '@/components/VoronoiBackground';
 
 function AboutPage() {
     return <>
+        <VoronoiBackground />
         <div className="grid grid-cols-3 gap-4 font-primary">
             <div>
                 <img src={myPicture} alt='Pietro picture'/>
             </div>
             <div className='col-span-2'>
                 <h1 className="font-bold">Pietro Gori</h1>
-                <div className="text-2xl">description</div>
+                <div className="text-2xl">Full Stack Developer</div>
+                <div className="text-xl my-4">pietro_gori@hotmail.com</div>
+                <div>I’m a Brazilian full stack developer with over 9 years of professional experience, both in Brazil and abroad, specializing in web development. I mainly work with JavaScript frameworks such as Angular, Vue, and React, and I also build REST APIs using Laravel and Django.</div>
+                <div>I created this website to enhance my professional visibility and as a personal lab, where I share content about programming and run experiments to improve my skills.</div>
+                <div>Outside of work, I practice Brazilian jiu-jitsu and have a passion for martial arts and combat sports. I’d be happy to help bring your project vision to life.</div>
             </div>
         </div>
-        <div className="text-2xl mt-28">description</div>
     </>
 }
 
