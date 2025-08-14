9b63730b0956cc4fc97ce3dfa1ac4d02ad3e65d9
Pietro <pietro_gori@hotmail.com>
2025-08-14 19:03:21 -0300

    - added icon; - removed comments; - other adjustments;

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,31 +1,13 @@
 // import { motion } from "motion/react"
 import VoronoiBackground from "@/components/VoronoiBackground"
+import AboutPage from "@/pages/about/AboutPage";
 
 function HomePage() {
-    // return <h1 className="text-3xl font-bold text-blue-800">Hello there, you are at the home page.</h1>
     return <>
         <VoronoiBackground />
-        <main className="relative z-10">
-            <h1 className="text-white text-4xl p-8 font-primary">Pietro Gori</h1>
-        </main>
+        <AboutPage />
     </>
 }
 
-// function Rotate() {
-//     return (
-//         <motion.div
-//             style={box}
-//             animate={{ rotate: 360 }}
-//             transition={{ duration: 1 }}
-//         />
-//     )
-// }
-
-// const box = {
-//     width: 100,
-//     height: 100,
-//     backgroundColor: "#ff0088",
-//     borderRadius: 5,
-// }
 
 export default HomePage;
