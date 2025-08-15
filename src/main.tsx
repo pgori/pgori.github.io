2081129c1e0261ca8929453287af8cee8193bb4e
Pietro <pietro_gori@hotmail.com>
2025-08-15 17:51:55 -0300

    declared module fontsource to typescript understand;

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import '@fontsource/vt323' //importing here as well to force vite to build the woff files
 import './index.css'
-import './styles/fonts.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
