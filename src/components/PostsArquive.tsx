38c8943f8e1869058dd0a07ba5eceb5c525b6fd2
Pietro <pietro_gori@hotmail.com>
2025-10-06 23:57:35 -0300

    blog page work in progress

--- src/components/PostsArquive.tsx
+++ src/components/PostsArquive.tsx
@@ -1,17 +1,26 @@
 import { API_URLS } from "@/config/api"
+import noImage from "@/assets/images/no_image.png"
 
 type data = {
     title: string;
     content: string;
     slug: string;
+    cover_image: string
 };
 
 export default function PostsArquive({menuItems}: {menuItems: Array<data>}) {
     return <>
-        <div className="relative col-start-1 row-span-full row-start-1 max-lg:hidden">
-            <ul className="px-5 mt-4 space-y-2 text-lg md:text-3xl">
-                {menuItems?.map((item, index) => (
-                    <li key={index}>
+        <div className="w-screen">
+            {menuItems?.map((item, index) => (
+                <div key={index} className="flex flex-col md:flex-row w-full max-w-3xl rounded-2xl mx-auto shadow-md overflow-hidden">
+                    <div className="md:w-1/2 w-full">
+                        <img 
+                            src={item.cover_image || noImage}
+                            alt="alguma coisa"
+                            className="w-full h-full object-cover" 
+                        />
+                    </div>
+                    <div className="md:w-1/2 w-full flex flex-col justify-between p-6">
                         <a 
                             href={`${API_URLS.posts + item.slug} `}
                             target="_blank"
@@ -19,9 +28,9 @@ export default function PostsArquive({menuItems}: {menuItems: Array<data>}) {
                         >
                             {item.title}  
                         </a>
-                    </li>
-                ))}
-            </ul>
+                    </div>
+                </div>
+            ))}
         </div>
     </>
 }
--- src/pages/blog/BlogPage.tsx
+++ src/pages/blog/BlogPage.tsx
@@ -8,6 +8,7 @@ type data = {
     title: string;
     content: string;
     slug: string;
+    cover_image: string;
 };
 
 export default function BlogPage() {
@@ -38,7 +39,7 @@ export default function BlogPage() {
     if (loading) return <p className={ isDarkTheme ? 'text-matrix' : ''}>Loading...</p>
 
     return <>
-        <div className={"grid min-h-dvh grid-cols-1 grid-rows-[1fr_1px_auto_1px_auto] pt-26.25 lg:grid-cols-[var(--container-2xs)_2.5rem_minmax(0,1fr)_2.5rem] lg:pt-14.25 xl:grid-cols-[var(--container-2xs)_2.5rem_minmax(0,1fr)_2.5rem]" + (isDarkTheme ? 'text-matrix' : '')}>
+        <div className={isDarkTheme ? 'text-matrix' : ''}>
             <PostsArquive menuItems={data}/>
             {/* <ul className="space-y-2">
                 {data?.map((item, index) => (
